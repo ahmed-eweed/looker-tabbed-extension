3edb0007b404bcdd37a15764b2e11f93e13cf7bf
ahmedeweed <ahmed.eweed@4sale.tech>
2025-08-11 16:41:15 +0300

    push

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import React from "react";
 import { Tabs } from "./Tabs";
 
 const App = () => {
-  return <Tabs />;
+  return 
+    <div>
+      <h1>Hello from Looker Extension</h1>
+    </div>
 };
 
 export default App;
